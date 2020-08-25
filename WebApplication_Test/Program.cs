b76df3b48169ecf8a042458a8d50d6bd0eec3138
Malte Weber <malte.weber@PCSMGmbH.onmicrosoft.com>
2020-08-25 12:02:08 +0200

    ddfdf

--- WebApplication_Test/Program.cs
+++ WebApplication_Test/Program.cs
@@ -13,6 +13,8 @@ namespace WebApplication_Test
     {
         public static void Main(string[] args)
         {
+            int a;
+
             CreateHostBuilder(args).Build().Run();
         }
 
